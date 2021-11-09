e2daf9f64b4348207144bcad3b5f3cb827745771
R Sadhukhan <93507446+SadhukhanR@users.noreply.github.com>
2021-11-09 22:34:59 +0530

    Update sud.py

--- sud.py
+++ sud.py
@@ -1,4 +1,14 @@
+#this is Demo Python
+import sys
 a=eval(input('a:'))
 b=eval(input('b:'))
-c = a + b
-print("Sum is",c))
+c = eval(input('e:'))
+e = a + b
+print("Sum is",e))
+if c == e:
+      print("a + b = c")
+      sys.exit()
+if c < e :
+      print("a + b > c")
+if c > e :
+      print("a + b < c")
